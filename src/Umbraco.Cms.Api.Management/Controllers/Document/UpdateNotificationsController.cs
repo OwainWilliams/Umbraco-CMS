232789d5187eaecc4a94ec1dfaafab2471b15e49
Owain Williams <owaingdwilliams@gmail.com>
2025-11-06 14:42:27 +0000

    [AMEND] Update Notifications Summary and Desc

--- src/Umbraco.Cms.Api.Management/Controllers/Document/UpdateNotificationsController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Document/UpdateNotificationsController.cs
@@ -26,8 +26,9 @@ public class UpdateNotificationsController : DocumentControllerBase
     [HttpPut("{id:guid}/notifications")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
-    [EndpointSummary("Updates a document.")]
-    [EndpointDescription("Updates a document identified by the provided Id with the details from the request model.")]
+    [EndpointSummary("Updates notifications for a document.")]
+    [EndpointDescription("Updates the notification settings for a document identified by the provided Id, allowing users to subscribe/unsubscribe to specific notification actions.")]
+   
     public async Task<IActionResult> UpdateNotifications(CancellationToken cancellationToken, Guid id, UpdateDocumentNotificationsRequestModel updateModel)
     {
         IContent? content = await _contentEditingService.GetAsync(id);
